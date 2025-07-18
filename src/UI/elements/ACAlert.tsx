222a7788447689240b2808b17adfdf544452edec
Daniele <daniele_comandini@hotmail.com>
2025-07-18 11:27:40 +0100

    ACAlert

--- src/UI/elements/ACAlert.tsx
+++ src/UI/elements/ACAlert.tsx
@@ -1,15 +1,24 @@
 import type { ReactNode } from 'react';
-import { Alert } from '@mui/material';
+import { Alert, type AlertProps } from '@mui/material';
 import type { ACIconProps } from './ACIcon';
 
 export interface ACAlertProps {
   children: ReactNode;
   severity: 'success' | 'error' | 'info' | 'warning';
   icon?: ACIconProps;
+  variant?: AlertProps['variant'];
 }
 
-const ACAlert: React.FC<ACAlertProps> = ({ children, severity = 'success' }) => {
-  return <Alert severity={severity || 'success'}>{children}</Alert>;
+const ACAlert: React.FC<ACAlertProps> = ({
+  children,
+  severity = 'success',
+  variant = 'standard',
+}) => {
+  return (
+    <Alert severity={severity || 'success'} variant={variant}>
+      {children}
+    </Alert>
+  );
 };
 
 export default ACAlert;

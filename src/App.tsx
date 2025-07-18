02f8dcdaec57989246c28108a1ca9972c396c5f7
Daniele <daniele_comandini@hotmail.com>
2025-07-18 12:33:37 +0100

    ACChips

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
+import { Home } from 'lucide-react';
+import ACChip from './UI/elements/ACChip';
 import ACCol from './UI/elements/ACCol';
 import ACRow from './UI/elements/ACRow';
 
 function App() {
   return (
-    <div
-      style={{
-        border: '2px solid red',
-      }}
-    >
-      <ACRow container spacing={2}>
-        <ACCol size={8}>Alto sinistra</ACCol>
-        <ACCol size={4}>Alto destra</ACCol>
-        <ACCol size={4}>Basso sinistra</ACCol>
-        <ACCol size={8}>Basso destra</ACCol>
-      </ACRow>
+    <div>
+      <ACChip icon={Home} iconPosition="right" color="red">
+        Message here!
+      </ACChip>
     </div>
   );
 }
---
+++ src/UI/elements/ACChip.tsx
@@ -0,0 +1,45 @@
+import { Chip } from '@mui/material';
+import type React from 'react';
+import type { ComponentType, ReactNode } from 'react';
+import ACIcon from './ACIcon';
+
+export interface ACChipProps {
+  children: ReactNode;
+  color?: string;
+  icon?: ComponentType;
+  iconPosition?: 'left' | 'right';
+}
+
+const ACChip: React.FC<ACChipProps> = ({
+  children,
+  color = 'black',
+  icon,
+  iconPosition = 'left',
+}) => {
+  const label = (
+    <>
+      {icon && iconPosition === 'left' && (
+        <ACIcon size={12} icon={icon} color={color} sx={{ marginRight: '5px' }} />
+      )}
+      {children}
+      {icon && iconPosition === 'right' && (
+        <ACIcon size={12} icon={icon} color={color} sx={{ marginLeft: '5px' }} />
+      )}
+    </>
+  );
+
+  return (
+    <Chip
+      label={label}
+      sx={{
+        color: color, // colore testo
+        borderColor: color, // colore bordo uguale al testo
+        borderStyle: 'solid',
+        borderWidth: 1,
+      }}
+      variant="outlined" // serve il bordo visibile
+    />
+  );
+};
+
+export default ACChip;

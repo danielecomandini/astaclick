898615a60a5d811e6b71ba9225bb5b914075286e
Daniele <daniele_comandini@hotmail.com>
2025-07-22 11:09:24 +0100

    ACPalette

---
+++ src/UI/themes/ACPalette.tsx
@@ -0,0 +1,29 @@
+import type { PaletteColor } from '@mui/material';
+
+interface PaletteColors {
+  PRIMARY: string;
+  SECONDARY: string;
+}
+
+interface InputColors {
+  COLOR: string;
+  BG_COLOR: string;
+  OPACITY?: number;
+}
+
+interface Palette {
+  DEFAULT: PaletteColors;
+  INPUT: InputColors;
+}
+
+export const ASTA_CLICK_PALETTE: Palette = {
+  DEFAULT: {
+    PRIMARY: '#0066EE',
+    SECONDARY: '#EE7700',
+  },
+  INPUT: {
+    BG_COLOR: '#FFAA00',
+    COLOR: '#0000AA',
+    OPACITY: 33,
+  },
+};
